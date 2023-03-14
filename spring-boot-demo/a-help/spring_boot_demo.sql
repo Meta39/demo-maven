a9b60ea4815444f37f46ed0cda9488206d6c1d3c
Meta39 <53995537@qq.com>
2023-03-14 13:14:20 +0800

    1. RBAC权限管理模型优化

--- spring-boot-demo/a-help/spring_boot_demo.sql
+++ spring-boot-demo/a-help/spring_boot_demo.sql
@@ -3,15 +3,15 @@
 
  Source Server         : localhost
  Source Server Type    : MySQL
- Source Server Version : 80031
+ Source Server Version : 80032
  Source Host           : localhost:3306
  Source Schema         : spring_boot_demo
 
  Target Server Type    : MySQL
- Target Server Version : 80031
+ Target Server Version : 80032
  File Encoding         : 65001
 
- Date: 11/03/2023 17:53:56
+ Date: 14/03/2023 13:14:10
 */
 
 SET NAMES utf8mb4;
@@ -24,30 +24,38 @@ DROP TABLE IF EXISTS `authorize`;
 CREATE TABLE `authorize`  (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
   `p_id` int UNSIGNED NOT NULL COMMENT 'parentId',
-  `authorize_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '权限名称（后端授权）',
-  `authorize_name_cn` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '权限名称中文（前端展示）',
+  `authorize_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '权限名称（前后端授权）',
+  `authorize_name_cn` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '权限名称中文（前端）',
+  `node_type` tinyint NOT NULL COMMENT '节点类型（1：文件夹 2：页面 3：按钮）',
+  `icon_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '图标地址（前端）',
+  `sort` int UNSIGNED NOT NULL COMMENT '排序号',
+  `link_url` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '页面对应的地址（前端）',
+  `level` int UNSIGNED NOT NULL COMMENT '层次（和节点类型不一样）',
+  `is_delete` int UNSIGNED NULL DEFAULT 0 COMMENT '删除（0：否 NULL：是）',
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `authorize_name`(`authorize_name` ASC) USING BTREE COMMENT '权限名称唯一索引'
-) ENGINE = InnoDB AUTO_INCREMENT = 16 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '权限' ROW_FORMAT = DYNAMIC;
+  UNIQUE INDEX `is_delete_and_authorize_name`(`is_delete` ASC, `authorize_name` ASC) USING BTREE COMMENT 'is_delete、authorize_name唯一组合索引',
+  INDEX `sort`(`node_type` ASC, `sort` ASC, `level` ASC) USING BTREE COMMENT '普通组合索引排序'
+) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '权限' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of authorize
 -- ----------------------------
-INSERT INTO `authorize` VALUES (1, 0, 'user', '用户管理');
-INSERT INTO `authorize` VALUES (2, 1, 'user:insert', '新增用户');
-INSERT INTO `authorize` VALUES (3, 1, 'user:select', '查询用户');
-INSERT INTO `authorize` VALUES (4, 1, 'user:update', '修改用户');
-INSERT INTO `authorize` VALUES (5, 1, 'user:delete', '删除用户');
-INSERT INTO `authorize` VALUES (6, 0, 'role', '角色管理');
-INSERT INTO `authorize` VALUES (7, 6, 'role:insert', '新增角色');
-INSERT INTO `authorize` VALUES (8, 6, 'role:select', '查询角色');
-INSERT INTO `authorize` VALUES (9, 6, 'role:update', '修改角色');
-INSERT INTO `authorize` VALUES (10, 6, 'role:delete', '删除角色');
-INSERT INTO `authorize` VALUES (11, 0, 'authorize', '权限管理');
-INSERT INTO `authorize` VALUES (12, 11, 'authorize:insert', '新增权限');
-INSERT INTO `authorize` VALUES (13, 11, 'authorize:select', '查询权限');
-INSERT INTO `authorize` VALUES (14, 11, 'authorize:update', '修改权限');
-INSERT INTO `authorize` VALUES (15, 11, 'authorize:delete', '删除权限');
+INSERT INTO `authorize` VALUES (1, 0, 'system_setting', '系统配置', 1, NULL, 1, NULL, 1, 0);
+INSERT INTO `authorize` VALUES (2, 1, 'user', '用户管理', 2, NULL, 1, NULL, 2, 0);
+INSERT INTO `authorize` VALUES (3, 2, 'user:insert', '用户管理-新增', 3, NULL, 1, NULL, 3, 0);
+INSERT INTO `authorize` VALUES (4, 2, 'user:select', '用户管理-查询', 3, NULL, 2, NULL, 3, 0);
+INSERT INTO `authorize` VALUES (5, 2, 'user:update', '用户管理-编辑', 3, NULL, 3, NULL, 3, 0);
+INSERT INTO `authorize` VALUES (6, 2, 'user:delete', '用户管理-删除', 3, NULL, 4, NULL, 3, 0);
+INSERT INTO `authorize` VALUES (7, 1, 'role', '角色管理', 2, NULL, 1, NULL, 2, 0);
+INSERT INTO `authorize` VALUES (8, 7, 'role:insert', '角色管理-新增', 3, NULL, 1, NULL, 3, 0);
+INSERT INTO `authorize` VALUES (9, 7, 'role:select', '角色管理-查询', 3, NULL, 2, NULL, 3, 0);
+INSERT INTO `authorize` VALUES (10, 7, 'role:update', '角色管理-编辑', 3, NULL, 3, NULL, 3, 0);
+INSERT INTO `authorize` VALUES (11, 7, 'role:delete', '角色管理-删除', 3, NULL, 4, NULL, 3, 0);
+INSERT INTO `authorize` VALUES (12, 1, 'authorize', '权限管理', 2, NULL, 1, NULL, 2, 0);
+INSERT INTO `authorize` VALUES (13, 12, 'authorize:insert', '权限管理-新增', 3, NULL, 1, NULL, 3, 0);
+INSERT INTO `authorize` VALUES (14, 12, 'authorize:select', '权限管理-查询', 3, NULL, 2, NULL, 3, 0);
+INSERT INTO `authorize` VALUES (15, 12, 'authorize:update', '权限管理-编辑', 3, NULL, 3, NULL, 3, 0);
+INSERT INTO `authorize` VALUES (16, 12, 'authorize:delete', '权限管理-删除', 3, NULL, 4, NULL, 3, 0);
 
 -- ----------------------------
 -- Table structure for role
@@ -56,17 +64,17 @@ DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role`  (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
   `role_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色名称',
-  `is_ban` int NOT NULL COMMENT '禁用角色（0：否 1：是）',
   `create_time` datetime NOT NULL COMMENT '创建时间',
   `update_time` datetime NOT NULL COMMENT '更新时间',
+  `is_delete` int UNSIGNED NULL DEFAULT 0 COMMENT '删除（0：否 NULL：是）',
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `role_name`(`role_name` ASC) USING BTREE COMMENT '角色名称唯一索引'
+  UNIQUE INDEX `is_delete_and_role_name`(`is_delete` ASC, `role_name` ASC) USING BTREE COMMENT 'is_delete、role_name唯一组合索引'
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '角色' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of role
 -- ----------------------------
-INSERT INTO `role` VALUES (1, '超级管理员', 0, '2023-03-11 13:58:25', '2023-03-11 13:58:27');
+INSERT INTO `role` VALUES (1, '超级管理员', '2023-03-11 13:58:25', '2023-03-11 13:58:27', 0);
 
 -- ----------------------------
 -- Table structure for role_authorize
@@ -91,23 +99,25 @@ CREATE TABLE `user`  (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'id',
   `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户名',
   `salt` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '盐',
-  `pwd` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '密码',
+  `pwd` char(172) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '密码',
   `nickname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '昵称',
   `sex` int UNSIGNED NOT NULL COMMENT '性别（0：女 1：男）',
   `create_time` datetime NOT NULL COMMENT '创建时间',
   `update_time` datetime NOT NULL COMMENT '更新时间',
-  `is_ban` int UNSIGNED NOT NULL COMMENT '禁用（0：否 1：是）',
-  `is_delete` int UNSIGNED NOT NULL COMMENT '删除（0：否 1：是）',
   `last_login_time` datetime NOT NULL COMMENT '最后登录时间',
+  `is_ban` int UNSIGNED NOT NULL COMMENT '禁用（0：否 1：是）',
+  `is_delete` int UNSIGNED NULL DEFAULT 0 COMMENT '删除（0：否 null：是）',
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `username`(`username` ASC) USING BTREE COMMENT 'username唯一索引'
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户' ROW_FORMAT = DYNAMIC;
+  UNIQUE INDEX `is_delete_and_username`(`is_delete` ASC, `username` ASC) USING BTREE COMMENT 'is_delete、username唯一组合索引'
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'admin', 'salt', 'password', '超级管理员', 0, '2023-03-10 15:42:10', '2023-03-10 15:42:13', 0, 0, '2023-03-10 15:42:20');
-INSERT INTO `user` VALUES (2, 'user', 'salt', 'user', '普通用户', 0, '2023-03-10 18:19:51', '2023-03-10 18:19:53', 0, 0, '2023-03-10 18:20:03');
+INSERT INTO `user` VALUES (1, 'admin', 'salt', 'UyxF2P7oMr5LnxM4tmeq7Jq2cVXnQ+H9jWwLzwWjn8qL+veKNlHjoom6wHIFXl7ez8JVksKg0/+/j18U/IPiXezYTWlNsqQ4IE/VPWxpo/WG4idKAj8dCUVa2AK4A5fXj4GJFZKi95Rp8vZAzJbyp6QecMiG04XSqFiFNLFWlf4=', '超级管理员', 0, '2023-03-10 15:42:10', '2023-03-10 15:42:13', '2023-03-10 15:42:20', 0, 0);
+INSERT INTO `user` VALUES (2, 'user', 'salt', 'Ju1u+exzxkzm+8Vy34A5deJLMgyt5LVKSJAlp4iYsQr/sq2nYwO9Dq3hqdNP8o0IBOAVfzTtHo0vZLJdfrJauf2FR5NkYziXiWEtXDS2i8ccYXWE2CKfVQ16eJ7tDl//AlNLQU6lwi6Z4Emn9EmIbOSrkPCFtmpRxjV5bHupqjQ=', '普通用户', 0, '2023-03-10 18:19:51', '2023-03-10 18:19:53', '2023-03-10 18:20:03', 0, NULL);
+INSERT INTO `user` VALUES (3, 'user', 'salt', 'Ju1u+exzxkzm+8Vy34A5deJLMgyt5LVKSJAlp4iYsQr/sq2nYwO9Dq3hqdNP8o0IBOAVfzTtHo0vZLJdfrJauf2FR5NkYziXiWEtXDS2i8ccYXWE2CKfVQ16eJ7tDl//AlNLQU6lwi6Z4Emn9EmIbOSrkPCFtmpRxjV5bHupqjQ=', '普通用户', 0, '2023-03-10 18:19:51', '2023-03-10 18:19:53', '2023-03-10 18:20:03', 0, 0);
+INSERT INTO `user` VALUES (5, 'user', 'salt', 'Ju1u+exzxkzm+8Vy34A5deJLMgyt5LVKSJAlp4iYsQr/sq2nYwO9Dq3hqdNP8o0IBOAVfzTtHo0vZLJdfrJauf2FR5NkYziXiWEtXDS2i8ccYXWE2CKfVQ16eJ7tDl//AlNLQU6lwi6Z4Emn9EmIbOSrkPCFtmpRxjV5bHupqjQ=', '普通用户', 0, '2023-03-10 18:19:51', '2023-03-10 18:19:53', '2023-03-10 18:20:03', 0, NULL);
 
 -- ----------------------------
 -- Table structure for user_role
@@ -124,5 +134,20 @@ CREATE TABLE `user_role`  (
 -- Records of user_role
 -- ----------------------------
 INSERT INTO `user_role` VALUES (1, 1, 1);
+
+-- ----------------------------
+-- Function structure for authorizeFunction
+-- ----------------------------
+DROP FUNCTION IF EXISTS `authorizeFunction`;
+delimiter ;;
+CREATE FUNCTION `authorizeFunction`(authorizeName VARCHAR(255))
+ RETURNS int
+BEGIN 
+  DECLARE result INT;
+  SELECT id INTO result FROM authorize WHERE authorize_name = authorizeName;
+  RETURN result;
+END
+;;
+delimiter ;
 
 SET FOREIGN_KEY_CHECKS = 1;
