9f2d0dae390deba442800ecb1f134a6b0a563c48
Meta39 <53995537@qq.com>
2023-11-17 09:50:35 +0800

    设置User新增为数据库自增主键

--- spring-boot-dynamic-data-source/src/main/java/com/fu/springbootdynamicdatasource/entity/User.java
+++ spring-boot-dynamic-data-source/src/main/java/com/fu/springbootdynamicdatasource/entity/User.java
@@ -1,11 +1,12 @@
 package com.fu.springbootdynamicdatasource.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 @Data
 public class User {
-    @TableId
+    @TableId(type = IdType.AUTO)
     private Integer id;
     private String name;
 }
