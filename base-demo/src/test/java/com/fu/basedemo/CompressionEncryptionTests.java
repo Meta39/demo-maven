f72b642f6bfc901acb3dba740bd04072a04d299d
Meta39 <53995537@qq.com>
2024-03-29 16:04:12 +0800

    数据压缩/解压缩并进行AES加/解密（设置字节编码为UTF-8）

--- base-demo/src/test/java/com/fu/basedemo/CompressionEncryptionTests.java
+++ base-demo/src/test/java/com/fu/basedemo/CompressionEncryptionTests.java
@@ -9,6 +9,7 @@ import javax.crypto.spec.SecretKeySpec;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
@@ -21,10 +22,10 @@ public class CompressionEncryptionTests {
     @Test
     void test(){
         String originalData = "那么，为什么要压缩任何东西？ 很简单，因为这是减少必须通过网络传送或存储到磁盘的数据量的好方法，因此可以提高操作速度。 根据文档的性质，典型的文本文件或消息可以减少10倍或更多。 当然，您将不得不考虑压缩和解压缩的成本，但是当您拥有大量数据时，这些成本将不会很大。";
-        System.out.println("未压缩byte[]长度：" + originalData.getBytes().length);
+        System.out.println("未压缩byte[]长度：" + originalData.getBytes(StandardCharsets.UTF_8).length);
 
-        // 压缩
-        byte[] compressedData = compress(originalData.getBytes());
+        // 压缩（设置字节编码为UTF-8）
+        byte[] compressedData = compress(originalData.getBytes(StandardCharsets.UTF_8));
         System.out.println("压缩后byte[]长度：" + compressedData.length);
 
         // 加密
@@ -37,8 +38,8 @@ public class CompressionEncryptionTests {
         // 解压缩
         byte[] decompressedData = decompress(decryptedData);
 
-        // 将解压缩后的字节转换回字符串并打印
-        String result = new String(decompressedData);
+        // 将解压缩后的字节转换回字符串并打印（设置字节编码为UTF-8）
+        String result = new String(decompressedData, StandardCharsets.UTF_8);
         System.out.println("解密数据：" + result);
     }
 
@@ -67,16 +68,18 @@ public class CompressionEncryptionTests {
     // 加密数据
     public static byte[] encrypt(byte[] data, String key) throws Exception {
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), "AES");
-        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, new IvParameterSpec(new byte[16]));
+        byte[] keyBytes = key.getBytes();
+        SecretKeySpec secretKeySpec = new SecretKeySpec(keyBytes, "AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, new IvParameterSpec(keyBytes));
         return cipher.doFinal(data);
     }
 
     // 解密数据
     public static byte[] decrypt(byte[] encryptedData, String key) throws Exception {
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), "AES");
-        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, new IvParameterSpec(new byte[16]));
+        byte[] keyBytes = key.getBytes();
+        SecretKeySpec secretKeySpec = new SecretKeySpec(keyBytes, "AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, new IvParameterSpec(keyBytes));
         return cipher.doFinal(encryptedData);
     }
 
