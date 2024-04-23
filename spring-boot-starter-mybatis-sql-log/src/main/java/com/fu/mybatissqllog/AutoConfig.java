5a11ac9574426645dc6e11304937ca78a92ef8ff
Meta39 <53995537@qq.com>
2024-04-23 13:33:44 +0800

    fix：mybatis日志输出bug修复

--- spring-boot-starter-mybatis-sql-log/src/main/java/com/fu/mybatissqllog/AutoConfig.java
+++ spring-boot-starter-mybatis-sql-log/src/main/java/com/fu/mybatissqllog/AutoConfig.java
@@ -1,15 +1,12 @@
 package com.fu.mybatissqllog;
 
-import com.fu.mybatissqllog.config.MyBatisSqlLogConfig;
 import com.fu.mybatissqllog.plugin.MyBatisSqlParsingPlugin;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties({MyBatisSqlLogConfig.class})
 //指定的参数值符合要求时，对应的配置才生效
 @ConditionalOnProperty(prefix = "mybatis-log", value = "enabled", havingValue = "true")
 public class AutoConfig {
--- spring-boot-starter-mybatis-sql-log/src/main/java/com/fu/mybatissqllog/plugin/MyBatisSqlParsingPlugin.java
+++ spring-boot-starter-mybatis-sql-log/src/main/java/com/fu/mybatissqllog/plugin/MyBatisSqlParsingPlugin.java
@@ -17,6 +17,7 @@ import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.type.TypeHandlerRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 
 import java.lang.reflect.Field;
 import java.sql.Statement;
@@ -24,6 +25,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
@@ -44,50 +46,37 @@ import java.util.Objects;
 })
 public final class MyBatisSqlParsingPlugin implements Interceptor {
     private static final Logger log = LoggerFactory.getLogger(MyBatisSqlParsingPlugin.class);
-    private static final Long DEFAULT_TIME_OUT = 5000L;
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
-        long startTime = System.currentTimeMillis();
+        StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
+        ParameterHandler parameterHandler = statementHandler.getParameterHandler();
+        BoundSql boundSql = statementHandler.getBoundSql();
+        String sql = boundSql.getSql();
         try {
-            return invocation.proceed();
-        } finally {
-            long endTime = System.currentTimeMillis();
-            long elapsedTime = endTime - startTime;
-            String sql = formatSql(invocation);
-            printColorString(String.format("执行SQL:\n%s", sql), elapsedTime);
+            log.info("Execute SQL:\n{}", formatSql(parameterHandler, boundSql, sql));
+        } catch (Exception e) {
+            log.error("SQL:{}\nformatSql Exception:", sql,e);
         }
+        return invocation.proceed();
     }
 
     /**
      * 格式化SQL及其参数
-     *
-     * @param invocation invocation
-     * @return java.lang.String
-     * @author liekkas 2020/12/08 10:43
      */
-    private String formatSql(Invocation invocation) throws NoSuchFieldException, IllegalAccessException {
-        //获取StatementHandler
-        StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
-        //获取ParameterHandler
-        ParameterHandler parameterHandler = statementHandler.getParameterHandler();
-        //获取boundSql
-        BoundSql boundSql = statementHandler.getBoundSql();
+    private String formatSql(ParameterHandler parameterHandler,BoundSql boundSql,String sql) throws NoSuchFieldException, IllegalAccessException {
 
         Class<? extends ParameterHandler> parameterHandlerClass = parameterHandler.getClass();
         Field mappedStatementField = parameterHandlerClass.getDeclaredField("mappedStatement");
         mappedStatementField.setAccessible(true);
         MappedStatement mappedStatement = (MappedStatement) mappedStatementField.get(parameterHandler);
 
-        // 元SQL
-        String sql = boundSql.getSql();
-        // 定义一个没有替换过占位符的sql，用于出异常时返回
-        String sqlWithoutReplacePlaceholder = sql;
+        sql = sql.replaceAll("\\s+", " ");
 
         // 输入sql字符串空判断
-        if (Objects.isNull(sql) || sql.trim().isEmpty()) {
+        if (!StringUtils.hasText(sql)) {
             return "";
         }
 
@@ -95,109 +84,59 @@ public final class MyBatisSqlParsingPlugin implements Interceptor {
         Object parameterObject = parameterHandler.getParameterObject();
         List<ParameterMapping> parameterMappingList = boundSql.getParameterMappings();
         if (Objects.isNull(parameterObject) || parameterMappingList.isEmpty()) {
-            return beautifySql(sql);
+            return sql;
         }
 
-        try {
-            sql = beautifySql(handleCommonParameter(boundSql, mappedStatement));
-        } catch (Exception e) {
-            log.error("替换SQL异常", e);
-            log.error("SQL参数：{}", parameterObject);
-            // 占位符替换过程中出现异常，则返回没有替换过占位符但是格式美化过的sql
-            return sqlWithoutReplacePlaceholder;
-        }
-
-        return sql;
+        return handleCommonParameter(boundSql, mappedStatement);
     }
 
-    /**
-     * 美化SQL
-     *
-     * @param sql sql
-     * @return java.lang.String
-     * @author liekkas 2020/12/08 10:45
-     */
-    private String beautifySql(String sql) {
-        sql = sql.replaceAll("[\\s\\n\\t]+", " ");
-        return sql;
-    }
-
-    /**
-     * 替换SQL中的?,设置sql参数
-     *
-     * @param boundSql boundSql
-     * @param mappedStatement mappedStatement
-     * @return java.lang.String
-     * @author liekkas 2020/12/08 10:46
-     */
+    //替换预编译SQL
     private String handleCommonParameter(BoundSql boundSql, MappedStatement mappedStatement) {
-
         String sql = boundSql.getSql();
         Object parameterObject = boundSql.getParameterObject();
         List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
         Configuration configuration = mappedStatement.getConfiguration();
         TypeHandlerRegistry typeHandlerRegistry = configuration.getTypeHandlerRegistry();
+        List<String> params = new ArrayList<>();
 
         for (ParameterMapping parameterMapping : parameterMappings) {
-            if (parameterMapping.getMode() != ParameterMode.OUT) {
-                Object value;
-                String propertyName = parameterMapping.getProperty();
-                if (boundSql.hasAdditionalParameter(propertyName)) {
-                    value = boundSql.getAdditionalParameter(propertyName);
-                } else if (parameterObject == null) {
-                    value = null;
-                } else if (typeHandlerRegistry.hasTypeHandler(parameterObject.getClass())) {
-                    value = parameterObject;
-                } else {
-                    MetaObject metaObject = configuration.newMetaObject(parameterObject);
-                    value = metaObject.getValue(propertyName);
-                }
-                sql = replacePlaceholder(sql, value);
+            if (parameterMapping.getMode() == ParameterMode.OUT) {
+                continue;
+            }
+            Object propertyValue;
+            String propertyName = parameterMapping.getProperty();
+            if (boundSql.hasAdditionalParameter(propertyName)) {
+                propertyValue = boundSql.getAdditionalParameter(propertyName);
+            } else if (parameterObject == null) {
+                params.add("null");
+                continue;
+            } else if (typeHandlerRegistry.hasTypeHandler(parameterObject.getClass())) {
+                propertyValue = parameterObject;
+            } else {
+                MetaObject metaObject = configuration.newMetaObject(parameterObject);
+                propertyValue = metaObject.getValue(propertyName);
+            }
+            //把参数放入list
+            if (propertyValue instanceof String) {
+                params.add("'" + propertyValue + "'");
+            } else if (propertyValue instanceof Date) {
+                params.add("'" + DATE_TIME_FORMATTER.format(((Date) propertyValue).toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()) + "'");
+            } else if (propertyValue instanceof LocalDate) {
+                params.add("'" + DATE_FORMATTER.format((LocalDate) propertyValue) + "'");
+            } else if (propertyValue instanceof LocalDateTime) {
+                params.add("'" + DATE_TIME_FORMATTER.format((LocalDateTime) propertyValue) + "'");
+            } else {
+                params.add(propertyValue.toString());
             }
         }
-        return sql;
+
+        //转译百分号
+        if (sql.contains("%")) {
+            sql = sql.replaceAll("%", "%%");
+        }
+
+        sql = sql.replaceAll("\\?", "%s");
+        return String.format(sql, params.toArray());
     }
 
-    /**
-     * 根据不同的propertyValue类型,匹配SQL?的类型并替换值
-     *
-     * @param sql sql
-     * @param propertyValue propertyValue
-     * @return java.lang.String
-     * @author liekkas 2020/12/08 10:48
-     */
-    private String replacePlaceholder(String sql, Object propertyValue) {
-        String value;
-        if (Objects.isNull(propertyValue)) {
-            value = "null";
-            return sql.replaceFirst("\\?", value);
-        }
-        if (propertyValue instanceof String) {
-            value = "'" + propertyValue + "'";
-        } else if (propertyValue instanceof Date) {
-            value = "'" + DATE_TIME_FORMATTER.format(((Date) propertyValue).toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()) + "'";
-        } else if (propertyValue instanceof LocalDate) {
-            value = "'" + DATE_FORMATTER.format((LocalDate) propertyValue) + "'";
-        } else if (propertyValue instanceof LocalDateTime) {
-            value = "'" + DATE_TIME_FORMATTER.format((LocalDateTime) propertyValue) + "'";
-        } else {
-            value = propertyValue.toString();
-        }
-        return sql.replaceFirst("\\?", value);
-    }
-
-    /**
-     * 根据不同的超时时间打印不同颜色的字体，若超时时间大于默认的超时时间，打印红色字体，否则打印黄色字体
-     *
-     * @param str Str
-     * @param timeOut 超时时间
-     * @author liekkas 2020/12/08 10:50
-     */
-    private void printColorString(String str, Long timeOut) {
-        if (timeOut < DEFAULT_TIME_OUT) {
-            log.info("{}", str);
-        } else {
-            log.error("{}", str);
-        }
-    }
 }
