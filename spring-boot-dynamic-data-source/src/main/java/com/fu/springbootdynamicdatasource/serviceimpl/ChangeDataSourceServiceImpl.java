f94b4bc7c8cd78bb067a28d8c91b3cc48dc4a75d
Meta39 <53995537@qq.com>
2024-05-10 16:42:04 +0800

    加了 @Transactional后，@ChangeDataSource注解失效无法切换数据源。

--- spring-boot-dynamic-data-source/src/main/java/com/fu/springbootdynamicdatasource/serviceimpl/ChangeDataSourceServiceImpl.java
+++ spring-boot-dynamic-data-source/src/main/java/com/fu/springbootdynamicdatasource/serviceimpl/ChangeDataSourceServiceImpl.java
@@ -10,7 +10,10 @@ public class ChangeDataSourceServiceImpl {
     private final ChangeDataSourceOne changeDataSourceOne;
     private final ChangeDataSourceTwo changeDataSourceTwo;
 
-    @Transactional//默认抛出RuntimeException才会回滚事务，如果抛出的是Exception则不会回滚事务！
+    /**
+     * TODO 加了 @Transactional后，无法切换数据源了。
+     */
+    @Transactional(rollbackFor = Exception.class)//默认抛出RuntimeException才会回滚事务，如果抛出的是Exception则不会回滚事务！
     public void change(){
         changeDataSourceOne.useDefaultTransactionManager();
         changeDataSourceTwo.useSecondTransactionManager();
