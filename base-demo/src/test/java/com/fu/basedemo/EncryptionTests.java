8dd610a7e29cdf6f670176170d7858c6ab15f850
Meta39 <53995537@qq.com>
2024-05-14 21:33:44 +0800

    前后端分离压缩、（RSA + AES）加密传输内容

--- base-demo/src/test/java/com/fu/basedemo/EncryptionTests.java
+++ base-demo/src/test/java/com/fu/basedemo/EncryptionTests.java
@@ -17,6 +17,17 @@ import java.security.spec.X509EncodedKeySpec;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
+/**
+ * 前后端分离压缩、（RSA + AES）加密传输内容
+ * 前端：
+ * 1、前端先对数据进行压缩，生成 AES 对称密钥对压缩数据进行 AES 对称加密，把加密内容放到请求体如：body : 值为 AES 加密后的密文
+ * 2、用后端提供 RSA 公钥对生成的 AES 对称密钥加密，并把 RSA 公钥加密后的 AES 加密密钥放到请求头/请求体，如：aes-key : 值是 RSA 公钥加密后的 AES 密钥密文
+ * 后端：
+ * 1、生成 RSA 非对称密钥对（公/私钥），自己保留私钥，公钥给前端，
+ * 让前端自己生成 AES 对称密钥，然后通过自己提供的 RSA 公钥对 AES 对称密钥进行加密，
+ * 并把加密后的 AES 密钥密文放到请求头/请求体传输到后端，后端用 RSA 私钥进行解密获取 AES 对称密钥对加密内容进行解密
+ * 2、解压缩传输内容
+ */
 public class EncryptionTests {
 
     /**
@@ -75,15 +86,25 @@ public class EncryptionTests {
 
     /**
      * 生成AES对称密钥
+     * AES（Advanced Encryption Standard）支持三种密钥长度：128位、192位和256位。
+     * 一般来说，更长的密钥长度提供了更高的安全性，因为它们增加了密码破解所需的时间和计算资源。
+     * 128位密钥长度通常被认为是安全的，并且在许多情况下被广泛使用。它提供了足够的安全性，适用于大多数应用场景。
+     * 192位和256位密钥长度提供了更高级别的安全性，但也需要更多的计算资源来加密和解密数据。这些长度通常用于对高度敏感的数据进行加密，或者在对抗更强大的攻击者时需要更高级别的安全性。
+     * 因此，选择哪个密钥长度取决于您的安全需求和性能考量。对于大多数情况，128位的密钥长度已经足够安全。
      */
     private static SecretKey generateAESKey() throws Exception {
         KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-        keyGenerator.init(256);
+        keyGenerator.init(128);
         return keyGenerator.generateKey();
     }
 
     /**
      * 生成RSA非对称密钥对
+     * RSA（Rivest-Shamir-Adleman）是一种非对称加密算法，它使用公钥和私钥进行加密和解密。
+     * 1024位：不推荐使用，因为它的安全性已经较低，容易受到现代计算能力的攻击。
+     * 2048位：目前被认为是最低安全性要求的长度，适用于大多数情况。
+     * 3072位或4096位：提供更高级别的安全性，适用于对安全性要求较高的场景，如对长期数据保密性要求较高的情况。
+     * 综上所述，对于大多数情况，2048位的RSA密钥长度是一个合适的选择，它提供了较高的安全性并且在性能方面也具有良好的平衡。如果您对安全性要求更高，可以考虑使用更长的密钥长度。
      */
     private static KeyPair generateRSAKeyPair() throws Exception {
         KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
