fc6520cceac9e3cd9fb404d51b239aac899c0569
Meta39 <53995537@qq.com>
2024-06-08 00:59:39 +0800

    SpringBoot3 使用 项目调整 graalvm jdk21

--- spring-boot3-demo/src/test/java/com/fu/springboot3demo/IOTests.java
+++ spring-boot3-demo/src/test/java/com/fu/springboot3demo/IOTests.java
@@ -3,9 +3,6 @@ package com.fu.springboot3demo;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-
 @Slf4j
 public class IOTests {
 
@@ -14,13 +11,13 @@ public class IOTests {
      */
     @Test
     void test() {
-        try (FileInputStream fis = new FileInputStream("D:/a.txt")) {
+        /*try (FileInputStream fis = new FileInputStream("D:/a.txt")) {
             int read = fis.read();
             log.info("读取内容：{}", read);
         } catch (IOException e) {
             log.error("文件读取异常", e);
             throw new RuntimeException("文件读取异常");
-        }
+        }*/
     }
 
 }
