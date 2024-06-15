f6572f8d52fcfa16bb8b5201a3824d631102f53a
Meta39 <53995537@qq.com>
2024-06-15 11:21:05 +0800

    使用IDEA JPA Buddy 插件检查 JPA 并修复JPA可能导致的问题

--- spring-boot-jpa-demo/src/main/java/com/fu/springbootjpademo/entity/Person.java
+++ spring-boot-jpa-demo/src/main/java/com/fu/springbootjpademo/entity/Person.java
@@ -1,15 +1,20 @@
 package com.fu.springbootjpademo.entity;
 
-import lombok.Data;
+import lombok.*;
 import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.Table;
+import org.hibernate.proxy.HibernateProxy;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 /**
  * 人员
  */
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 @Table(appliesTo = "person",comment = "人员表")//appliesTo指表的名称（必填），MySQL表名小写，用Person会抛异常。
 public class Person {
@@ -28,4 +33,19 @@ public class Person {
     @Transient//此注解表示数据库不创建该字段
     private String ignore;//忽略字段
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Person person = (Person) o;
+        return getId() != null && Objects.equals(getId(), person.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
--- spring-boot-jpa-demo/src/main/java/com/fu/springbootjpademo/repositories/PersonRepository.java
+++ spring-boot-jpa-demo/src/main/java/com/fu/springbootjpademo/repositories/PersonRepository.java
@@ -2,7 +2,7 @@ package com.fu.springbootjpademo.repositories;
 
 import com.fu.springbootjpademo.entity.Person;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -17,6 +17,6 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
 
     List<Person> findByNameLike(String name);
 
-    Page<Person> findAllByNameLike(String name, PageRequest pageRequest);
+    Page<Person> findAllByNameLike(String name, Pageable pageable);
 
 }
