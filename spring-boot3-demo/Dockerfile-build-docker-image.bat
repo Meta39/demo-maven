08cb60c8a8f1996b6ebccaa1ab4e90143193ddff
Meta39 <53995537@qq.com>
2024-06-16 16:26:39 +0800

    maven打包的时候使用离线办公模式，加速打包

--- spring-boot3-demo/Dockerfile-build-docker-image.bat
+++ spring-boot3-demo/Dockerfile-build-docker-image.bat
@@ -5,7 +5,7 @@ set THREAD=1C
 set JAVA_HOME=C:/Users/X/.jdks/graalvm-jdk-21.0.3
 
 echo mvn clean package ......
-start /B /WAIT cmd /c "mvn -T %THREAD% -D maven.test.skip=true -P %PROFILES% clean package -q -f pom.xml"
+start /B /WAIT cmd /c "mvn -T %THREAD% -D maven.test.skip=true -P %PROFILES% clean package -q -o -f pom.xml"
 @REM 如果cmd执行失败，则后续的命令不会执行。
 if %ERRORLEVEL% neq 0 (
     echo Error: mvn clean package failed.
@@ -13,14 +13,14 @@ if %ERRORLEVEL% neq 0 (
 )
 
 echo mvn dockerfile:build ......
-start /B /WAIT cmd /c "mvn -T %THREAD% dockerfile:build -q -f pom.xml"
+start /B /WAIT cmd /c "mvn -T %THREAD% dockerfile:build -q -o -f pom.xml"
 if %ERRORLEVEL% neq 0 (
     echo Error: mvn dockerfile:build failed.
     exit /b %ERRORLEVEL%
 )
 
 echo mvn dockerfile:push ......
-start /B /WAIT cmd /c "mvn dockerfile:push -q -f pom.xml"
+start /B /WAIT cmd /c "mvn dockerfile:push -q -o -f pom.xml"
 if %ERRORLEVEL% neq 0 (
     echo Error: mvn dockerfile:push failed.
     exit /b %ERRORLEVEL%
