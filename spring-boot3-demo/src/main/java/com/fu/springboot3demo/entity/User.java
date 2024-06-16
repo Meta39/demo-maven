2a65a4e2029b64237c1e00f43bedab0ea8b91ca0
Meta39 <53995537@qq.com>
2024-06-16 23:06:47 +0800

    增加测试 MyBatis-Plus 测试

--- spring-boot3-demo/src/main/java/com/fu/springboot3demo/entity/User.java
+++ spring-boot3-demo/src/main/java/com/fu/springboot3demo/entity/User.java
@@ -1,12 +1,17 @@
 package com.fu.springboot3demo.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.fu.springboot3demo.annotations.Desensitize;
 import com.fu.springboot3demo.enums.Desensitization;
 import lombok.Data;
 
 @Data
+@TableName("user")
 public class User {
 
+    @TableId(type = IdType.INPUT)
     private Long id;//ID
 
     @Desensitize(Desensitization.USERNAME)//姓名脱敏
---
+++ spring-boot3-demo/src/main/java/com/fu/springboot3demo/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.fu.springboot3demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.fu.springboot3demo.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
