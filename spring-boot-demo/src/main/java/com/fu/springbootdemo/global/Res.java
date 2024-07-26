c5b86f06aa6fee754d040831cc1af04c35a4c9dd
Meta39 <53995537@qq.com>
2024-07-26 11:55:12 +0800

    统一返回类优化

--- spring-boot-demo/src/main/java/com/fu/springbootdemo/global/Res.java
+++ spring-boot-demo/src/main/java/com/fu/springbootdemo/global/Res.java
@@ -17,27 +17,27 @@ public class Res<T> implements Serializable {
 
     private T data; //数据
 
-    private Res (){}//私有化构造方法
+    private Res() {
+    }//私有化构造方法
 
-    private static final  Res res = new Res<>();//私有化静态实例化类
+    private static final Res res = new Res<>();//私有化静态实例化类
 
     /**
      * 成功（前端没有特别要求状态码，直接用这个。）
+     *
      * @param data 数据
      */
-    public static <T> Res<T> ok(T data){
-        res.setCode(1);
-        res.setMsg("成功");
-        res.setData(data);
-        return res;
+    public static <T> Res<T> ok(T data) {
+        return ok("success", data);
     }
 
     /**
      * 成功（注意：前端要求返回信息要具体的才用这个，否则不使用！）
+     *
      * @param message 成功信息（注意：前端要求返回信息要具体的才用这个，否则不使用！）
-     * @param data 数据
+     * @param data    数据
      */
-    public static <T> Res<T> ok(String message,T data){
+    public static <T> Res<T> ok(String message, T data) {
         res.setCode(1);
         res.setMsg(message);
         res.setData(data);
@@ -46,21 +46,20 @@ public class Res<T> implements Serializable {
 
     /**
      * 一般异常（注意：前端没有特别要求状态码，直接用这个。）
+     *
      * @param errorMessage 异常信息
      */
-    public static <T> Res<T> err(String errorMessage){
-        res.setCode(0);
-        res.setMsg(errorMessage);
-        res.setData(null);//设置为null防止上次数据没有清空
-        return res;
+    public static <T> Res<T> err(String errorMessage) {
+        return err(0, errorMessage);
     }
 
     /**
      * 全局捕获异常（注意：前端要求状态码为其它的才使用这个，否则不使用。）
-     * @param code 状态码（注意：前端无特殊要求不要使用这个！就算要使用也要在Code枚举类里枚举出来，方便到时候统一修改！）
+     *
+     * @param code         状态码（注意：前端无特殊要求不要使用这个！就算要使用也要在Code枚举类里枚举出来，方便到时候统一修改！）
      * @param errorMessage 错误信息
      */
-    public static <T> Res<T> err(Integer code,String errorMessage){
+    public static <T> Res<T> err(Integer code, String errorMessage) {
         res.setCode(code);
         res.setMsg(errorMessage);
         res.setData(null);//设置为null防止上次数据没有清空
