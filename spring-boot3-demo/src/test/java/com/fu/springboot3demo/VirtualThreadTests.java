2e520cd1fa27d76f0c77fb81808fb786a2be82aa
Meta39 <53995537@qq.com>
2024-08-08 19:28:25 +0800

    虚拟线程join()函数的作用：内部虚拟线程阻塞外部虚拟线程，并且直到内部虚拟线程执行完，才继续往下执行外部虚拟线程的剩余代码。注意：即使内部虚拟线程抛出异常，也不会阻止外部虚拟线程继续往下执行。因为内部虚拟线程抛出的异常不会往外部虚拟线程抛！！！

--- spring-boot3-demo/src/test/java/com/fu/springboot3demo/VirtualThreadTests.java
+++ spring-boot3-demo/src/test/java/com/fu/springboot3demo/VirtualThreadTests.java
@@ -1,5 +1,6 @@
 package com.fu.springboot3demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
@@ -9,11 +10,11 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 
-
 /**
  * JDK21虚拟线程测试类
  * 创建日期：2024-05-09
  */
+@Slf4j
 public class VirtualThreadTests {
 
     /**
@@ -67,11 +68,38 @@ public class VirtualThreadTests {
     }
 
     /**
+     * 虚拟线程join()函数的作用：内部虚拟线程阻塞外部虚拟线程，并且直到内部虚拟线程执行完，才继续往下执行外部虚拟线程的剩余代码。
+     * 注意：即使内部虚拟线程抛出异常，也不会阻止外部虚拟线程继续往下执行。因为内部虚拟线程抛出的异常不会往外部虚拟线程抛！！！
+     */
+    @Test
+    void testVirtualThread5() throws InterruptedException {
+        Thread.startVirtualThread(() -> {
+            System.out.printf("0.我是虚拟线程？%s\n", Thread.currentThread().isVirtual());
+            System.out.printf("1.我是第1个虚拟线程%s...\n", Thread.currentThread().threadId());
+            Thread.startVirtualThread(() -> System.out.printf("3.我是第2个虚拟线程%s...\n", Thread.currentThread().threadId()));
+            System.out.printf("2.不阻塞第1个虚拟线程，不等第2个虚拟线程执行完，第1个虚拟线程继续往下执行剩余代码，当前虚拟线程ID:%s\n", Thread.currentThread().threadId());
+        });
+        Thread.sleep(1000L);//休眠1秒，查看结果。
+        System.out.println("======== 分割线 ========");
+        Thread.startVirtualThread(() -> {
+            System.out.printf("1.我是第1个虚拟线程%s...\n", Thread.currentThread().threadId());
+            Thread thread2 = Thread.startVirtualThread(() -> System.out.printf("2.我是第2个虚拟线程%s...\n", Thread.currentThread().threadId()));
+            try {
+                //等待线程2执行完，才往下执行。
+                thread2.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.printf("3.阻塞第1个虚拟线程，等第2个虚拟线程执行完，第1个虚拟线程才继续往下执行剩余代码，当前线程ID:%s\n", Thread.currentThread().threadId());
+        });
+    }
+
+    /**
      * 结构化并发（预览功能）
      * 当一个子任务发生错误时，其它的子任务会在未完成的情况下取消
      */
     @Test
-    void testVirtualThread5() throws InterruptedException {
+    void testStructuredTaskScope() throws InterruptedException {
         /*try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
             StructuredTaskScope.Subtask<String> fork1 = scope.fork(VirtualThreadTests::task1);
             StructuredTaskScope.Subtask<String> fork2 = scope.fork(VirtualThreadTests::task2);
